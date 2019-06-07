d47d3ee388af37a94d40ce8024e909f3aaac3db0
Wheeler Weise <wheeler.weise@gmail.com>
2019-06-07 14:33:06 -0500

    Added issues and bitter/sourness selector.

--- src/NewShotLogEntryForm.js
+++ src/NewShotLogEntryForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import DropDownWithAddButton from './DropDownWithAddButton/DropDownWithAddButton';
+import DropDown from './DropDown/DropDown';
 import TextInputWithUnits from './TextInputWithUnits';
+import { string } from 'prop-types';
 
 class NewShotLogEntryForm extends React.Component {
     constructor(props) {
@@ -16,6 +18,24 @@ class NewShotLogEntryForm extends React.Component {
             "Counter culture": ["Apollo", "Hologram"],
             "Starbucks": ["House blend", "Yukon"]
         };
+
+        this.issues = [
+            "Spritzers",
+            "Extraction too fast"
+        ];
+
+        let bitter_sour_vals = [];
+        for (let i = 5; i >= -5; --i) {
+            if (i > 0) {
+                bitter_sour_vals.push("+" + i.toString() + " (bitter)");
+            } else if (i < 0) {
+                bitter_sour_vals.push(i.toString() + " (sour)");
+            } else {
+                bitter_sour_vals.push(i.toString());
+            }
+        }
+
+        this.bitter_sour_vals = bitter_sour_vals;
         
         this.initializeFormState();
     }
@@ -28,7 +48,9 @@ class NewShotLogEntryForm extends React.Component {
                 grinder_setting: "",
                 dose_amount_grams: 0,
                 brew_amount_grams: 0,
-                brew_time_seconds: 0 
+                brew_time_seconds: 0,
+                bitter_sour: "",
+                issues: ""
             }
         }
     }
@@ -39,31 +61,43 @@ class NewShotLogEntryForm extends React.Component {
                 <DropDownWithAddButton 
                     items={this.roasters} 
                     name="Roaster" 
-                    onChange={(val) => this.handleRoasterChanged(val)}
+                    onChange={val => this.handleValueChange("roaster", val)}
                     onNewItemAdded={(newItem) => this.handleNewRoasterAdded(newItem)}
                     onStartAddNewItem={() => this.handleOnStartAddingNewRoaster()} />
                 {this.renderBeansDropDownIfRoasterSelected()}
                 <TextInputWithUnits
+                    name="Grinder setting"
+                    unit=""
+                    value={this.state.form.grinder_setting}
+                    onChange={val => this.handleValueChange("grinder_setting", val)} />
+                <TextInputWithUnits
                     name="Dose amount"
                     unit="grams"
                     value={this.state.form.dose_amount_grams}
-                    onChange={(val) => this.handleValueChange("dose_amount_grams", val)} />
+                    onChange={val => this.handleValueChange("dose_amount_grams", val)} />
                 <TextInputWithUnits
                     name="Brew amount"
                     unit="grams"
                     value={this.state.form.brew_amount_grams}
-                    onChange={(val) => this.handleValueChange("brew_amount_grams", val)} />
+                    onChange={val => this.handleValueChange("brew_amount_grams", val)} />
                 <TextInputWithUnits
                     name="Brew time"
                     unit="seconds"
                     value={this.state.form.brew_time_seconds}
-                    onChange={(val) => this.handleValueChange("brew_time_seconds", val)} />
+                    onChange={val => this.handleValueChange("brew_time_seconds", val)} />
+                <DropDown
+                    name="Bitterness/sourness"
+                    items={this.bitter_sour_vals}
+                    value={this.state.form.bitter_sour}
+                    onChange={val => this.handleValueChange("bitter_sour", val)} />
+                <DropDownWithAddButton
+                    name="Issues?"
+                    items={this.issues}
+                    value={this.state.form.issues}
+                    onChange={val => this.handleValueChange("issues", val)}
+                    onNewItemAdded={val => this.handleNewIssueAdded(val)} />
             </div>
         )
-    }
-
-    handleRoasterChanged(newVal) {
-        this.handleValueChange("roaster", newVal);
     }
 
     handleOnStartAddingNewRoaster() {
@@ -92,8 +126,8 @@ class NewShotLogEntryForm extends React.Component {
                 <DropDownWithAddButton
                     items={beansForRoaster}
                     name="Bean"
-                    onChange={(bean) => this.handleNewBeanSelected(bean)}
-                    onNewItemAdded={(bean) => this.handleNewBeanTypeAdded(bean)} />
+                    onChange={bean => this.handleValueChange("bean", bean)}
+                    onNewItemAdded={bean => this.handleNewBeanTypeAdded(bean)} />
             );
         }
 
@@ -107,6 +141,10 @@ class NewShotLogEntryForm extends React.Component {
     handleNewBeanTypeAdded(bean) {
         let beansForRoaster = this.beans[this.state.form.roaster];
         beansForRoaster.push(bean);
+    }
+
+    handleNewIssueAdded(issue) {
+        this.issues.push(issue);
     }
 
     handleValueChange(val_name, value) {
