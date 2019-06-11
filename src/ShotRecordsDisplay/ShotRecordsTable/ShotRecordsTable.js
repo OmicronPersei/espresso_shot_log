326e43662323bdadde34f4bb98539bc59c23e62a
Wheeler Weise <wheeler.weise@gmail.com>
2019-06-11 16:02:55 -0500

    Can now sort by either number or string.

--- src/ShotRecordsDisplay/ShotRecordsTable/ShotRecordsTable.js
+++ src/ShotRecordsDisplay/ShotRecordsTable/ShotRecordsTable.js
@@ -29,18 +29,18 @@ class ShotRecordsTable extends React.Component {
             },
             {
                 roaster: "Counter culture",
-                bean: "Apollo",
+                bean: "Bsdfasdf",
                 grinder_setting: "1.8",
                 dose_amount_grams: 18,
                 brew_amount_grams: 24,
                 brew_time_seconds: 36,
-                bitter_sour: "+3 (bitter)",
+                bitter_sour: "+33 (bitter)",
                 issues: "",
                 id: 2
             },
             {
                 roaster: "Counter culture",
-                bean: "Apollo",
+                bean: "Csfsdf",
                 grinder_setting: "1.8",
                 dose_amount_grams: 18,
                 brew_amount_grams: 24,
@@ -53,13 +53,13 @@ class ShotRecordsTable extends React.Component {
 
         //todo: refactor to not need the `id` prop here.
         this.cols = [
-            { label: "Roaster", sortable: true, id: "roaster" },
-            { label: "Bean", sortable: true, id: "bean" },
-            { label: "Dose", sortable: true, id: "dose_amount" },
-            { label: "Brew Amount", sortable: true, id: "brew_amount" },
-            { label: "Brew Ratio", sortable: true, id: "brew_ratio" },
-            { label: "Brew time", sortable: true, id: "brew_time_seconds" },
-            { label: "Bitterness/sourness", sortable: true, id: "bitter_sour" },
+            { label: "Roaster", sortable: true, id: "roaster", sortAsNumber: false },
+            { label: "Bean", sortable: true, id: "bean", sortAsNumber: false },
+            { label: "Dose", sortable: true, id: "dose_amount", sortAsNumber: true },
+            { label: "Brew Amount", sortable: true, id: "brew_amount", sortAsNumber: true },
+            { label: "Brew Ratio", sortable: true, id: "brew_ratio", sortAsNumber: true },
+            { label: "Brew time", sortable: true, id: "brew_time_seconds", sortAsNumber: true },
+            { label: "Bitterness/sourness", sortable: true, id: "bitter_sour", sortAsNumber: false },
         ];
         
         this.state = {
@@ -96,13 +96,36 @@ class ShotRecordsTable extends React.Component {
             return shotDisplayRecords;
         }
         let getValForSorting = val => val[this.state.sortedColId];
+        let sortAsNumber = this.cols.find(col => col.id === this.state.sortedColId).sortAsNumber;
+
         return shotDisplayRecords.sort((a,b) => {
             let aVal = getValForSorting(a);
             let bVal = getValForSorting(b);
-            if (this.state.sortOrder === "asc") {
-                return aVal - bVal;
+
+            if (sortAsNumber) {
+                if (this.state.sortOrder === "asc") {
+                    return aVal - bVal;
+                } else {
+                    return bVal - aVal;
+                }
             } else {
-                return bVal - aVal;
+                if (this.state.sortOrder === "asc") {
+                    if (aVal.toUpperCase() > bVal.toUpperCase()) {
+                        return 1;
+                    } else if (aVal.toUpperCase() < bVal.toUpperCase()) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    if (aVal.toUpperCase() < bVal.toUpperCase()) {
+                        return 1;
+                    } else if (aVal.toUpperCase() > bVal.toUpperCase()) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
             }
         });
     }
